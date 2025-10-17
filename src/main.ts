87af84934491cc293a1fb44d04508876346b1958
Kaviuk Vladyslav <149083065+kaviukvlad@users.noreply.github.com>
2025-10-17 11:53:03 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,13 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['http://localhost:3001'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
 	app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-  prefix: '/uploads/'
-});
+		prefix: '/uploads/'
+	})
 
 	const port = process.env.PORT || 3000
 
