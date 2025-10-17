c5a55aac87eb3f8af3277c06d0b0fa665aa2ffd9
Kaviuk Vladyslav <149083065+kaviukvlad@users.noreply.github.com>
2025-10-17 11:52:21 +0300

    Update prisma.service.ts

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,5 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { PrismaClient } from 'prisma/generated/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-	async onModuleInit() {
-		await this.$connect()
-	}
-}
+export class PrismaService extends PrismaClient {}
